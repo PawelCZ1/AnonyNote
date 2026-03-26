c5c32ec946d4e91a62890a3b04738b21f3095cbc
pawel <pawelczapran@gmail.com>
2026-03-26 21:07:16 +0100

    First commit

---
+++ src/main/java/pl/pawelcz/AnonyNote/AnonyNoteApplication.java
@@ -0,0 +1,13 @@
+package pl.pawelcz.AnonyNote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonyNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnonyNoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pawelcz/AnonyNote/AnonyNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pawelcz.AnonyNote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonyNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
